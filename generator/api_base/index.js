3abda37949600a17b44e32be5c9ec675cc37cd40
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-18 22:14:16 -0400

    Cleaned up new & edit pages

--- generator/api_base/index.js
+++ generator/api_base/index.js
@@ -9,9 +9,8 @@ module.exports = {
     )
 
     const userSchema = blueprint.schemas.find(s => s.identifier === 'user')
-    // const requiredUserAttributes = userSchema.attributes.filter(r => r.required)
-    const requiredUserAttributes = userSchema.attributes
-    const inlineDeconstrction = requiredUserAttributes.map(r => r.identifier).join(', ')
+    const inlineDeconstrction = userSchema.attributes.map(r => r.identifier).join(', ')
+
     await this.renderComponent({
       src: 'src/api/auth/auth.controller.js',
       dest: 'backend/src/api/auth/auth.controller.js',
--- generator/vue_store/templates/constants.js
+++ generator/vue_store/templates/constants.js
@@ -1,4 +1,3 @@
 export const API_ROOT = '/api/<%= schema.identifier_plural %>'
 
-// TODO - print out model prototype
 export const NEW_<%= schema.identifier.toUpperCase() %> = <%- newModel %>
