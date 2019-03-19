dccc0302588f14749288c398a76a5de73fb2cf78
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-18 22:10:31 -0400

    Removed unused Vuex mixins & mediator, added VueToasted

--- generator/vue_base/templates/src/App.vue
+++ generator/vue_base/templates/src/App.vue
@@ -2,22 +2,19 @@
   <div id="app">
     <Navbar/>
     <router-view />
-    <ToastList/>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
-import ToastList from '@/modules/toast/components/ToastList'
 
 export default {
   name: 'app',
   components: {
-    Navbar,
-    ToastList
+    Navbar
   },
   created () {
-    this.$store.dispatch('auth/fetchUserProfile') // Authenticates current user on application start
+    this.$store.dispatch('auth/profile/fetch') // Authenticates current user on application start
   },
   metaInfo: {
     title: 'Loading...', // set a title
@@ -40,6 +37,7 @@ export default {
 
   #app {
     height: 100%;
+    padding-top: 4.5rem;
   }
 
   .app {
