1338894561c7e818409c8548dcfb8c6fd577dcab
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-19 15:00:29 -0400

    Added mailer spec, full branch coverage in pagination spec

--- generator/api_base/templates/src/lib/pagination.spec.js
+++ generator/api_base/templates/src/lib/pagination.spec.js
@@ -1,14 +1,6 @@
 const { getPaginationParams } = require('./pagination')
 const assert = require('chai').assert;
 
-// Default pagination options
-// function getPaginationParams (req) {
-//   let page = Number(req.query.page) || 0;
-//   let per_page = Number(req.query.per_page) || 300;
-//   let offset = per_page * page;
-//   return { page, per_page, offset }
-// }
-
 // // // //
 
 // non_zero_query
@@ -18,16 +10,28 @@ const non_zero_query = {
   per_page: '10',
 }
 
+// empty_query
+// Should return an offset of 0
+const empty_query = {}
+
 describe('/lib/pagination.js', () => {
+
   describe('non-zero page parameter present', () => {
     it('verify correct offset', () => {
-
       let params = getPaginationParams({ query: non_zero_query })
-      // Asserts equality of original payload and signed/verified payload
       assert.equal(params.offset, 20);
       assert.equal(params.page, Number(non_zero_query.page))
       assert.equal(params.per_page, Number(non_zero_query.per_page));
-
     });
   });
+
+  describe('empty query', () => {
+    it('verify correct offset', () => {
+      let params = getPaginationParams({ query: empty_query })
+      assert.equal(params.offset, 0);
+      assert.equal(params.page, 0)
+      assert.equal(params.per_page, 10);
+    });
+  });
+
 });
