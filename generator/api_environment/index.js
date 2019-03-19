321913042fad42f32f326ff3d5c5e7bf3cedf3aa
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-19 14:49:27 -0400

    Updated Express ENV files, added test env

--- generator/api_environment/index.js
+++ generator/api_environment/index.js
@@ -2,6 +2,7 @@ module.exports = {
   name: 'NodeExpressEnvironment',
   async write () {
     await this.renderComponent({ src: 'env-dev.txt', dest: 'backend/.env' })
+    await this.renderComponent({ src: 'env-test.txt', dest: 'backend/.env.test' })
     await this.renderComponent({ src: 'env-docker.txt', dest: 'backend/.env.docker' })
   }
 }
