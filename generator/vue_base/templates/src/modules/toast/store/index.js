713436c2d955114b112e13fa6e3a8c7c279dc373
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-18 22:10:51 -0400

    Drastically simplified Toast Vuex module

--- generator/vue_base/templates/src/modules/toast/store/index.js
+++ generator/vue_base/templates/src/modules/toast/store/index.js
@@ -1,46 +1,25 @@
-import uniqueId from 'lodash/uniqueId'
-
-// removeToast
-// Helper function used to remove a toast from state.collection
-function removeToast (collection, id) {
-  return collection.filter(e => e.id !== id)
-}
+const duration = 3500
+const position = 'top-right'
+const theme = 'toasted-primary'
 
 export default {
   namespaced: true,
-  state: {
-    collection: [
-      // Notification examples:
-      // { dismissible: true, strong: 'Warning', message: 'Something went wrong.', context: 'warning' },
-      // { dismissible: true, strong: 'Info', message: 'Something went wrong.', context: 'info' },
-      // { dismissible: true, strong: 'Danger!', message: 'Something went wrong.', context: 'danger' }
-    ]
-  },
-  getters: {
-    collection: state => {
-      return state.collection
-    }
-  },
-  mutations: {
-    // Adds a toast to state.collection
-    add (state, toast) {
-      // Assigns unique ID to toast
-      // this is used for removing the toast after a timeout
-      let toastId = uniqueId()
-      toast.id = toastId
-
-      // Adds the toast to state.collection
-      state.collection.push(toast)
-
-      // Removes the toast after
-      setTimeout(() => {
-        state.collection = removeToast(state.collection, toastId)
-      }, toast.timeout || 2500)
+  actions: {
+    success ({}, { message }) {
+      this._vm.$toasted.show(message, {
+         theme,
+         position,
+         duration,
+         className: 'bg-success'
+      });
     },
-
-    // Removes a Toast from state.collection
-    remove (state, toastId) {
-      state.collection = removeToast(state.collection, toastId)
+    error ({}, { message }) {
+      this._vm.$toasted.show('Error! ' + message, {
+         theme,
+         position,
+         duration,
+         className: 'bg-danger'
+      });
     }
   }
 }
