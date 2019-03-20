4942ce5d0119dc0a3dea3296bea38637e5f69d5e
Alexander Schwartzberg <aeksco@gmail.com>
2019-03-19 20:09:07 -0400

    More API spec fixes

--- generator/api_resource_spec/templates/resource.spec.js
+++ generator/api_resource_spec/templates/resource.spec.js
@@ -10,18 +10,15 @@ describe('<%= schema.label %> API', () => {
 
   <%- helpers.indent(include('./partials/spec-list.js'), 2) %>
 
-  // // // //
   <%_ if (schema.identifier !== 'user') { _%>
   <%- helpers.indent(include('./partials/spec-create.js'), 2) %>
+  <%- helpers.indent(include('./partials/spec-update.js'), 2) %>
+  <%_ } _%>
 
   <%- helpers.indent(include('./partials/spec-show.js'), 2) %>
 
-  <%- helpers.indent(include('./partials/spec-update.js'), 2) %>
-
   <%- helpers.indent(include('./partials/spec-destroy.js'), 2) %>
-  <%_ } _%>
 
   <%- helpers.indent(include('./partials/spec-relation-actions.js'), 2) %>
-
 
 });
